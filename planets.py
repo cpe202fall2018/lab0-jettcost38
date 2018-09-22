34469b60005e2505720ae8593427d0e245e8df63
jettcost38 <jecost2@gmail.com>
2018-09-21 18:51:14 -0700

    World Series Game 1

--- planets.py
+++ planets.py
@@ -1,7 +1,17 @@
+# Name: Jett Costibolo
+# Student ID: jcostibo
+# Date: 21 September 2018
+#
+# Lab 0
+# Section 15
+# Purpose of Lab: To introduce and become familiar with GitHub and Python Syntax
+
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    pounds = float(input("What do you weigh on earth? "))
+    mars_pounds = pounds * 0.38
+    jupiter_pounds = pounds * 2.34
+    print("\nOn Mars you would weigh %.2f pounds.\nOn Jupiter you would weigh %.2f pounds. " % (mars_pounds, jupiter_pounds))
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
